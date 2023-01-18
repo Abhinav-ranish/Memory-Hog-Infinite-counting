70e67a99492171e03b7545b6338821abb7098fc3
Abhinav <abhinav.ranish@outlook.com>
2023-01-17 22:22:18 -0700

    Update and rename Help.java to Crash.java

--- Crash.java
+++ Crash.java
@@ -1,6 +1,6 @@
 import java.util.stream.IntStream;
 
-public class Help {
+public class Crash {
     public static void main(String[] args) {
         IntStream.iterate(0, i -> i + 1)
                  .forEach(System.out::println);
